d4173b8026541879bbe08231cf4c5047b42e0f3b
Alex Ostafi <133591634+urraccon@users.noreply.github.com>
2025-06-27 14:48:39 +0300

    update base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   plugins: [react(), svgr()],
 
-  base: "/taskpro-frontend",
+  base: "/taskpro",
 
   build: {
     outDir: "dist",
